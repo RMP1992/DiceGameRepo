29aa03234dc8b42530f4f758fd717008d4d3999c
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-01-30 10:39:53 +0000

    Toggle function working

--- app.js
+++ app.js
@@ -4,42 +4,45 @@
 
 // let randonHigh = Math.ceil(Math.random()* 6)
 // let randonLow = Math.floor(Math.random()* 6)
-const button = document.getElementById('myBtn');
-const submit = document.getElementById('button')
+const button = document.getElementById('button');
+// const submit = document.getElementById('button')
 const player1Message = document.getElementById('paragraph');
-const Player1Message2 = document.getElementById('divparagraph')
+const Player1Message2 = document.getElementById('divparagraph');
 const player1Message3 = document.getElementById('paradiv');
-const img = document.getElementById('dice1')
+const img1 = document.getElementById('dice1');
+const img2 = document.getElementById('dice2');
 const player2Message = document.getElementById('paragraph2');
-const Player2Message2 = document.getElementById('divparagraph2')
+const Player2Message2 = document.getElementById('divparagraph2');
 const player2Message3 = document.getElementById('paradiv2');
 
-let player1 = true
+let player;
 
-button.addEventListener('click', () =>{
+// button.addEventListener('click', () =>{
 
-   
-        if (randomHigh == 1) {
-        player1 == false
-        button.innerHTML = `<p>Player 2</p>`
-        console.log('this code is running')
-        }else {
-        player1 == true 
-        button.innerHTML = `<p>Player 1</p>`
-        console.log('this code is also running')
-        }
-})
+//     const playerToggle = ()=>{
+//         if (randomHigh == 1) {
+//         player1 == false
+//         button.textContent = 'Player 2'
+        
+//         console.log('this code is running')
+//         }else {
+//         player1 == true 
+//         button.textContent = 'Player 1'
+        
+        
+//         console.log('this code is also running')
+//         }
+//     }
+// })
 
 
 let randomHigh;
 let pointScored = 0;
+let player1 = 1
 
 button.addEventListener('click', ()=>{
     randomHigh = Math.ceil(Math.random()* 6);
-    img.src = `img/dice${randomHigh}.png`
-
     
-
     Player1Message2.innerHTML = `<p>You rolled ${randomHigh}</p>`
     pointScored += randomHigh
     player1Message3.innerHTML = `<p>The total score is ${pointScored}</p>`
@@ -52,6 +55,25 @@ button.addEventListener('click', ()=>{
     }else{
         player1Message.innerHTML = `<p>Lucky</p>`
     }
+    
+    const playerToggle = ()=>{
+        if (player1 == 1) {
+            
+            button.textContent = 'Player 2'
+            img2.src = `img/dice${randomHigh}.png`
+            player1 = 0
+            console.log('this code is running')
+        }else {
+            button.textContent = 'Player 1'
+            img1.src = `img/dice${randomHigh}.png`
+            player1 = 1
+        }
+    }
+    
+    playerToggle()
+
+    
+    
     
 });
 
